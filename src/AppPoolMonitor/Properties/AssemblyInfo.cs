2de609a7687ff81142dae8d1b92e40f1b9632947
Adam Humpherys <adam@adamh.us>
2025-07-29 07:46:28 -0600

    copyright/version bump

--- src/AppPoolMonitor/Properties/AssemblyInfo.cs
+++ src/AppPoolMonitor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppPoolMonitor")]
+[assembly: AssemblyTitle("Application Pool Monitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Manwaring Web Solutions, Inc")]
+[assembly: AssemblyCompany("Adam Humpherys")]
 [assembly: AssemblyProduct("AppPoolMonitor")]
-[assembly: AssemblyCopyright("Copyright © 2017 Manwaring Web Solutions")]
+[assembly: AssemblyCopyright("Copyright © 2025 Adam Humpherys")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
